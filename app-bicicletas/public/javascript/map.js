b8c2b6ff2d446308a9e58ba81ed840771ba35e8f
Osvaldo Rojas <o.rojas.g.2022@gmail.com>
2022-10-22 17:38:24 -0300

    Carga Template principal

---
+++ app-bicicletas/public/javascript/map.js
@@ -0,0 +1,12 @@
+var map = L.map("main-map").setView(
+	[-33.448922542593145, -70.64473916312011],
+	13
+);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+	maxZoom: 19,
+	attribution:
+		'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+L.marker([-33.4484106629084, -70.64208674232113]).addTo(map);
